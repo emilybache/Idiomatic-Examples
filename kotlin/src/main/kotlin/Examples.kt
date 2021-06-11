db39256dca94e63c02e6b8ce085992475ad59853
emily <emily@bacheconsulting.com>
2021-06-11 15:17:13 +0200

    even more idiomatic

--- kotlin/src/main/kotlin/Examples.kt
+++ kotlin/src/main/kotlin/Examples.kt
@@ -2,28 +2,22 @@ import java.util.stream.Collectors
 
 class Examples {
 
-    fun example1(name: String): String {
-        return "Hello, $name. Your name is ${name.length} characters long"
+    fun example1(name: String) = "Hello, $name. Your name is ${name.length} characters long"
+
+    fun example2(from: Int) = buildString {
+        for (i in from downTo 0) append("$i\n")
     }
 
-    fun example2(from: Int): String {
-        return buildString  {
-            for (i in from downTo 0) append("$i\n")
-        }
-    }
+    fun example3(domain: String) =
+        sequenceOf("joe@example.com", "nancy@example.com", "coach@sammancoaching.org")
+            .filter { domain in it }
+            .joinToString()
 
-    fun example3(domain: String): String {
-        val emailsList = listOf("joe@example.com", "nancy@example.com", "coach@sammancoaching.org")
-        return emailsList.filter { domain in it }.joinToString()
-    }
-
-    fun example4(colour: String): String {
-        return when (colour) {
-            "Red" -> "Warm"
-            "Green" -> "Happy"
-            "Blue" -> "Calm"
-            else -> throw IllegalArgumentException("Invalid color")
-        }
+    fun example4(colour: String) = when (colour) {
+        "Red" -> "Warm"
+        "Green" -> "Happy"
+        "Blue" -> "Calm"
+        else -> throw IllegalArgumentException("Invalid colour")
     }
 
 }
